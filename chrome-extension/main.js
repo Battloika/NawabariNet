47bf494196edd14a11c804f278cf0f91b08fcf45
Sesta <sesta.ec5@gmail.com>
2015-12-03 10:36:32 +0900

    ready painting funciton

--- chrome-extension/main.js
+++ chrome-extension/main.js
@@ -1,24 +1,42 @@
 $(function(){
+  var image_urls = {
+    red_ink: chrome.extension.getURL( "images/red_ink.png" ),
+    blue_ink: chrome.extension.getURL( "images/blue_ink.png" )
+  };
+
   chrome.runtime.onMessage.addListener( function( request ) {
-    console.log( request );
+    $( "body" ).append( $( "<div></div>", {
+      "id": "effect-area"
+    } ).css( {
+      "position": "absolute",
+      "top": "0",
+      "left": "0",
+      "width": "100%",
+      "height": "100%"
+    } ));
+
+    paint( 200, 100 );
   } );
 
-  function showComment( comment, nega_or_posi, pos_x, pos_y, index ){
-    var width = 100;
+  function paint( pos_x, pos_y ){
+    if( !pos_x ) pos_x = 100;
+    if( !pos_y ) pos_y = 100;
+
+    var ink_width = 100;
 
     var $div = $( "<div></div>", {
-      "class": "comment"
+      "class": "ink"
     } ).css( {
       "display": "none",
       "position": "absolute",
-      "top": pos_y + "px",
-      "left": pos_x + "px",
-      "width": width + "px"
+      "top": ( pos_y - ink_width / 2 ) + "px",
+      "left": ( pos_x - ink_width / 2 )  + "px",
+      "width": ink_width + "px"
     } );
 
     $div.append( $( "<img>", {
-      "src": image_urls[ nega_or_posi ],
-      "width": width + "px"
+      "src": image_urls[ "red_ink" ],
+      "width": ink_width + "px"
     } ).css( {
       "display": "block",
       "position": "absolute",
@@ -26,21 +44,9 @@ $(function(){
       "left": "0"
     } ) );
 
-    $div.append( $( "<div></div>" )
-    .css( {
-      "position": "absolute",
-      "top": "35px",
-      "left": "0",
-      "width": width + "px",
-      "color": "white",
-      "text-align": "center",
-      "font-family": "ヒラギノ角ゴ StdN"
-    } ).text( comment ) );
-
     $( "#effect-area" ).append( $div );
 
-    $div.delay( index * 50 )
-        .fadeIn( 1000 )
+    $div.fadeIn( 1000 )
         .delay( 800 )
         .fadeOut( 2000 );
   }
