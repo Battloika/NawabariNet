970e5c13c6b261285b0bcd805c1a995b06029643
suiyujin <suiyujin04@gmail.com>
2015-12-04 21:24:03 +0900

    generate model Domain

--- server/db/schema.rb
+++ server/db/schema.rb
@@ -11,6 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20151204122040) do
+
+  create_table "domains", force: :cascade do |t|
+    t.string "domain", limit: 255, null: false
+  end
 
 end
