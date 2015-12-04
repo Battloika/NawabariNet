5074fce931b724339b20097e6bea8bd828400905
Sesta <sesta.ec5@gmail.com>
2015-12-04 11:19:04 +0900

    keep paint mode

--- chrome-extension/browser_action.js
+++ chrome-extension/browser_action.js
@@ -1,4 +1,10 @@
 $( function(){
+    chrome.runtime.sendMessage( {
+      type: "get_mode"
+    }, function( response ){
+      $( "." + response + "-mode" ).prop( "checked", true );
+    });
+
   $( "input[name='paint_mode']" ).on( "change", function(){
     chrome.runtime.sendMessage( {
       type: "change_mode",
--- chrome-extension/extension_click_listener.js
+++ chrome-extension/extension_click_listener.js
@@ -1,10 +1,18 @@
-chrome.runtime.onMessage.addListener( function( request ){
+var mode = "none";
+
+chrome.runtime.onMessage.addListener( function( request, sender, sendResponse ){
+  if( request.type == "get_mode" ){
+    sendResponse( mode );
+    return;
+  }
+
   var queryInfo = {
     active: true,
     windowId: chrome.windows.WINDOW_ID_CURRENT
   };
 
   chrome.tabs.query( queryInfo, function( result ){
+    mode = request.mode;
     var currentTab = result.shift();
     chrome.tabs.sendMessage( currentTab.id, request, function(){} );
   } );
