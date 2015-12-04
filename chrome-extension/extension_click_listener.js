ef3bf7bc684535a8d8630e14bddf7ed22471eb05
Sesta <sesta.ec5@gmail.com>
2015-12-04 11:36:09 +0900

    add tab change and update event

--- chrome-extension/extension_click_listener.js
+++ chrome-extension/extension_click_listener.js
@@ -6,14 +6,33 @@ chrome.runtime.onMessage.addListener( function( request, sender, sendResponse ){
     return;
   }
 
+  mode = request.mode;
+  changePaintMode();
+} );
+
+chrome.tabs.onActivated.addListener(function(activeInfo) {
+  mode = "none";
+  changePaintMode();
+} );
+
+
+chrome.tabs.onUpdated.addListener(function(tabId, changeInfo) {
+  mode = "none";
+  changePaintMode();
+} );
+
+function changePaintMode(){
   var queryInfo = {
     active: true,
     windowId: chrome.windows.WINDOW_ID_CURRENT
   };
 
   chrome.tabs.query( queryInfo, function( result ){
-    mode = request.mode;
     var currentTab = result.shift();
-    chrome.tabs.sendMessage( currentTab.id, request, function(){} );
+    chrome.tabs.sendMessage( currentTab.id, {
+      type: "change_mode",
+      mode: mode,
+    } );
   } );
-} );
+
+}
