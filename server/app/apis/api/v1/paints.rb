046fb750e0b79a9bceb72fd89cb8befbfae65f87
suiyujin <suiyujin04@gmail.com>
2015-12-07 07:48:58 +0900

    post paints api

--- server/app/apis/api/v1/paints.rb
+++ server/app/apis/api/v1/paints.rb
@@ -2,10 +2,24 @@ module API
   module V1
     class Paints < Grape::API
       helpers do
-        # パラメータのチェック
+        def authenticate!
+          unless params[:api_key] == ENV.fetch('API_KEY')
+            error!('Unauthorized. Invalid key.', 401)
+          end
+        end
+
+        def calc_points(page, painted_map)
+          # 塗った割合(%)をpointとする
+          Paint.create({
+            point: (painted_map.flatten.count(1).to_f / painted_map.flatten.size.to_f * 100).round(1),
+            page_id: page.id
+          })
+        end
+
         params :attributes do
+          requires :api_key, type: String, desc: "API key."
           requires :url, type: String, desc: "Page url."
-          requires :painted_map, type: Array[Integer], desc: "Page painted_map."
+          requires :painted_map, type: ::Utils::PaintedMap, desc: "Page painted_map."
         end
       end
 
@@ -15,16 +29,28 @@ module API
           use :attributes
         end
         post '/' do
-          normalize_url = Page.normalize_url(params[:url])
+          authenticate!
 
-          page = Page.find_by(url: normalize_url)
+          normalize_url = Page.normalize_url(params[:url])
+          painted_map = params[:painted_map].value
+
+          page = Page.find_by(url: normalize_url.to_s)
           if page
-            Page.create({
-              url: normalize_url,
-              painted_map: painted_map
-            })
+            painted_map.each_with_index do |row, row_i|
+              row.each_with_index do |col, col_i|
+                page.painted_map[row_i][col_i] = 1 if col == 1
+              end
+            end
+            page.save
+            calc_points(page, painted_map)
           else
-            # 登録している場合：painted_mapをマージする
+            domain = Domain.find_or_create_by(domain: normalize_url.host)
+            page = Page.create({
+              url: normalize_url.to_s,
+              painted_map: painted_map,
+              domain_id: domain.id
+            })
+            calc_points(page, painted_map)
           end
         end
       end
