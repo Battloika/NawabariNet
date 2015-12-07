203810e9fee58f287e864d597248b0ecb9dd555e
suiyujin <suiyujin04@gmail.com>
2015-12-07 15:20:04 +0900

    modify display listing pages

--- server/app/controllers/pages_controller.rb
+++ server/app/controllers/pages_controller.rb
@@ -4,7 +4,7 @@ class PagesController < ApplicationController
   # GET /pages
   # GET /pages.json
   def index
-    @pages = Page.all
+    @pages = Page.includes(:domain).all
   end
 
   # GET /pages/1
