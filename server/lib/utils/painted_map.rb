3ff50d4c1afc5ec658b2394538518fbd5d88ba6e
suiyujin <suiyujin04@gmail.com>
2015-12-07 12:23:25 +0900

    convert string into array before validation for swagger

--- server/lib/utils/painted_map.rb
+++ server/lib/utils/painted_map.rb
@@ -5,6 +5,9 @@ class Utils::PaintedMap
   end
 
   def self.parse(value)
+    if value.instance_of?(String) && value.include?('[')
+      value = value[2..-3].split('],[').map { |row| row.split(',') }.map { |row| row.map(&:to_i) }
+    end
     unless value.instance_of?(Array) &&
       value.first.instance_of?(Array) &&
       value.first.first.instance_of?(Fixnum)
