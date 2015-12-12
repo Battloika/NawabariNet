007cf20a1280cc1873b3034c6ae9963b8c86210c
suiyujin <suiyujin04@gmail.com>
2015-12-12 09:15:46 +0900

    generate model user and social_profile

---
+++ server/app/models/user.rb
@@ -0,0 +1,33 @@
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :trackable, :omniauthable
+
+  has_many :social_profiles, dependent: :destroy
+
+  def social_profile(provider)
+    social_profiles.select{ |sp| sp.provider == provider.to_s }.first
+  end
+
+  def email_required?
+    false
+  end
+
+  def email_changed?
+    false
+  end
+
+  def password_required?
+    false
+  end
+
+  class << self
+    def current_user=(user)
+      Thread.current[:current_user] = user
+    end
+
+    def current_user
+      Thread.current[:current_user]
+    end
+  end
+end
