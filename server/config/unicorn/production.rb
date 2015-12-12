2c013debb9660b9bd8d2df362925c10998b31f22
suiyujin <suiyujin04@gmail.com>
2015-12-12 19:34:08 +0900

    add arguments of fork methods

--- server/config/unicorn/production.rb
+++ server/config/unicorn/production.rb
@@ -7,7 +7,7 @@ pid "#{ENV.fetch('DEPLOY_TO')}/shared/tmp/pids/unicorn.pid"
 
 working_directory "#{ENV.fetch('DEPLOY_TO')}/current"
 
-before_fork do
+before_fork do |server, worker|
   Signal.trap 'TERM' do
     logger.info('Unicorn master intercepting TERM and sending myself QUIT instead')
     Process.kill 'QUIT', Process.pid
@@ -16,7 +16,7 @@ before_fork do
   defined?(ActiveRecord::Base) && ActiveRecord::Base.connection.disconnect!
 end
 
-after_fork do
+after_fork do |server, worker|
   Signal.trap 'TERM' do
     logger.info('Unicorn worker intercepting TERM and doing nothing. Wait for master to send QUIT')
   end
